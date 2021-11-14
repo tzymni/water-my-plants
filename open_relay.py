474425ccc83857cb1354cfa6085a1315e5586e1b
tomasz.zymni <tomasz.zymni@gmail.com>
2021-11-14 15:08:37 +0100

    Created script to open relay on defined time. Refs #1

---
+++ open_relay.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import RPi.GPIO as GPIO
+import time
+import argparse
+
+# relay channel
+CHANNEL = 21
+
+# time to wait after open relay
+DELAY_TIME = 5
+
+# GPIO setup
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(CHANNEL, GPIO.OUT)
+
+def close_relay(pin):
+    print("Relay closed")    
+    GPIO.output(pin, GPIO.HIGH)  # Woda nie leci
+
+def open_relay(pin):
+    print("Relay open")
+    GPIO.output(pin, GPIO.LOW)  # Woda leci
+
+def parser():
+    parser = argparse.ArgumentParser(description='Open relay for defined time in open_time argument.')
+    parser.add_argument('--open_time', type=int,
+                    help='Time of open relay in seconds.')
+    parser.set_defaults(open_time=DELAY_TIME)
+    args = parser.parse_args()
+    return args
+
+if __name__ == '__main__':
+    try:    
+        args = parser()
+        print('Relay will be open for ' + str(args.open_time) + ' seconds')
+        print('Relay will be open after ' + str(DELAY_TIME) +' seconds')
+        time.sleep(DELAY_TIME)
+        open_relay(CHANNEL)
+        time.sleep(args.open_time)
+        close_relay(CHANNEL)
+        GPIO.cleanup()
+    except KeyboardInterrupt:
+        GPIO.cleanup()
